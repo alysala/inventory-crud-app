a651da610d19013ae439dc988b0ab56361853792
Alyssa Alameda <alyssa.alameda@toyota.com>
2023-06-28 16:16:57 -0700

    React setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <a href='https://pngtree.com/so/stack'>stack png from pngtree.com/</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
