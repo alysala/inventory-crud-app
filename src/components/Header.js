f580983d6fca81aeacb1aeb8b88adbcc0db723c4
Alyssa Alameda <alyssa.alameda@toyota.com>
2023-06-30 23:13:07 -0700

    Simple page navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
+	const navigate = useNavigate();
+
 	return (
         <div className={styles.baseHeader}>
 			<div>
-				<p className={styles.textRight}>Login</p>
-				<p className={styles.text}>June 2023 CRUD Application Submission</p>
+				<p className={styles.textRight} onClick={() => {navigate('/login', { replace: true })}}>Login</p>
+				<p className={styles.text} onClick={() => {navigate('/', { replace: true })}}>June 2023 CRUD Application Submission</p>
 			</div>
         </div>
 	);
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 import boxes from './stacked-boxes.png';
 
 const Home = () => {
+    const navigate = useNavigate();
+
 	return (
         <div className={styles.baseHome}>
             <div className={styles.leftSide}>
                 <p className={styles.titleText}>GOT INVENTORY?</p>
-                <button className={styles.browseButton}>Browse our public catalogue</button> 
+                <button className={styles.browseButton} onClick={() => {navigate('/inventory', { replace: true })}}>Browse our public catalogue</button> 
             </div>
             <div className={styles.rightSide}>
             <img id='boxes' src={boxes} alt='stacked boxes'/>
